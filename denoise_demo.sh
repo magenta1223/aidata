a3fe000f632c24c4553104e17eed3f6c4c41d87e
magenta1223 <magenta1223@naver.com>
2022-09-19 20:18:18 +0900

    shell script update

--- denoise_demo.sh
+++ denoise_demo.sh
@@ -1,7 +1,7 @@
 python test_single_img_integrated.py \
 --img demo/58.png \
 --Dconfig configs/denoise/GaussianColorDenoising_Restormer.yml \
---Dweights configs/deraining/deraining.pth \
+--Dweights configs/deraining/deblur.pth \
 --Sconfig configs/segmentation/pspnet_r50-d8_512x1024_40k_cityscapes.py \
 --Sweights configs/segmentation/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth \
 --device cuda:0 \
